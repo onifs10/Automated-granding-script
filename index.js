a47067f73c21b529d4041f3b49043149230e2049
tife <onifadebolu64@gmail.com>
2022-10-18 12:31:05 +0100

    fix

--- index.js
+++ index.js
@@ -13,19 +13,57 @@ const run = async () => {
     const passed = []
     const failed = [];
     const pending = [];
-
-    async function grade(username, link)  {
-        const browser = await puppeteer.launch()
+    const minimal_args = [
+  '--autoplay-policy=user-gesture-required',
+  '--disable-background-networking',
+  '--disable-background-timer-throttling',
+  '--disable-backgrounding-occluded-windows',
+  '--disable-breakpad',
+  '--disable-client-side-phishing-detection',
+  '--disable-component-update',
+  '--disable-default-apps',
+  '--disable-dev-shm-usage',
+  '--disable-domain-reliability',
+  '--disable-extensions',
+  '--disable-features=AudioServiceOutOfProcess',
+  '--disable-hang-monitor',
+  '--disable-ipc-flooding-protection',
+  '--disable-notifications',
+  '--disable-offer-store-unmasked-wallet-cards',
+  '--disable-popup-blocking',
+  '--disable-print-preview',
+  '--disable-prompt-on-repost',
+  '--disable-renderer-backgrounding',
+  '--disable-setuid-sandbox',
+  '--disable-speech-api',
+  '--disable-sync',
+  '--hide-scrollbars',
+  '--ignore-gpu-blacklist',
+  '--metrics-recording-only',
+  '--mute-audio',
+  '--no-default-browser-check',
+  '--no-first-run',
+  '--no-pings',
+  '--no-sandbox',
+  '--no-zygote',
+  '--password-store=basic',
+  '--use-gl=swiftshader',
+  '--use-mock-keychain',
+    ];
+    
+    async function grade(username, link) {
+        console.log(username,link)
+        const browser = await puppeteer.launch({headless: true, args: minimal_args})
         try {
             const page = await browser.newPage()
-            page.setDefaultTimeout(30 * 1000)
+            page.setDefaultTimeout(5 * 1000)
             await page.goto(link)
+            await page.screenshot({path: path.join(__dirname, `/image/${username}.png`), type: "png", fullPage:true});
             let grade = 0;
             const getElement = async (selector) => {
-                return await page.evaluate(elementSelector => {
-                    return document.querySelector(elementSelector);
-                }, selector);
-            }
+                return await page.$(selector);
+            };
+            
             // Extract the results from the page.
             const profilePicture = await getElement("#profile__img");
             if(profilePicture){
@@ -40,34 +78,48 @@ const run = async () => {
                 grade += 20;
             }
             // click the zuri button
-            if(await  getElement("#btn__zuri")) {
-                await Promise.all([
-                    page.waitForNavigation(),
-                    page.click('#btn__zuri'),
-                ]);
-                if(page.url().includes("training.zuri.team")){
-                    grade += 20;
-                    await page.goBack();
+            if (await getElement("#btn__zuri")) {
+                await page.click('#btn__zuri');
+                grade += 10;
+                try {
+                    await Promise.all([
+                        page.waitForNavigation(),
+                        page.click('#btn__zuri'),
+                    ]);
+                    if (page.url().includes("training.zuri.team")) {
+                        grade += 10;
+                        await page.goBack();
+                        // }
+                    }
+                } catch (e) {
+                    // ignore error
+                }
+                
+            }
+                
+            if (await getElement("#books")) {
+                grade += 10;
+                try {
+                    await Promise.all([
+                        page.waitForNavigation(),
+                        page.click('#books'),
+                    ]);
+                    if (page.url().includes("books.zuri.team")) {
+                        grade += 10;
+                        await page.goBack();
+                    }
+                } catch (e) {
+                    // igonre errror
                 }
             }
-            if(await  getElement("#books")){
-                await Promise.all([
-                    page.waitForNavigation(),
-                    page.click('#books'),
-                ]);
-                if(page.url().includes("books.zuri.team")){
-                    grade += 20;
-                    await page.goBack();
-                }
-            }
-
+            console.log(grade, username)
             if(grade >= 80){
                 passed.push(username)
             }else {
                 failed.push(username)
             }
         } catch (error) {
-            console.error(error)
+            console.log(error)
             pending.push({username, link})
         }
         await browser.close()
